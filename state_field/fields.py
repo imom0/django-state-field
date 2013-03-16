d34df33b3de402fae11696c4396bd6b83bac4007
iMom0 <mobeiheart@gmail.com>
2013-03-16 11:25:35 +0800

    Add statedescriptor

--- state_field/fields.py
+++ state_field/fields.py
@@ -4,6 +4,20 @@
 from django.db.models.fields import CharField
 
 
+class StateDescriptor(object):
+
+    def __init__(self, field):
+        self.field = field
+
+    def __get__(self, instance, instance_type=None):
+        if instance is None:
+            raise AttributeError('State must be accessed via instance')
+        return instance.__dict__[self.field.name]
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.field.name] = value
+
+
 class StateField(CharField):
 
     description = u'StateField'
