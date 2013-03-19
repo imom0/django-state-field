12355d54d7f7ec6c196a7ff073da085738de371d
iMom0 <mobeiheart@gmail.com>
2013-03-19 21:38:54 +0800

    Add test for default value

--- state_field/tests.py
+++ state_field/tests.py
@@ -1,16 +1,22 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.db import models
+
+from state_field.fields import StateField
+
+flow = {
+    'default_value': ['next', 'default_value', 'prev'],
+    'next': ['next', 'default_value']
+}
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class Book(models.Model):
+
+    name = models.CharField(max_length=20)
+    state = StateField(max_length=20, state_flow=flow, default='default_value')
+
+
+class StateFieldTest(TestCase):
+
+    def test_create_default_value(self):
+        book = Book.objects.create(name='book')
+        self.assertEqual(book.state, 'default_value')
