53eb4e967c6ddd2a542cad5406c0abbc6458d4cd
imom0 <mobeiheart@gmail.com>
2013-10-08 14:18:30 +0800

    Use mock for test hook called

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 from setuptools import setup, find_packages
 
+from .state_field import __version__
+
 install_requires = [
     'Django>=1.4.2',
+    'mock==1.0.1',
 ]
 
 setup(
     name='django-state-field',
-    version='0.3',
+    version=__version__,
     description='Django custom field for changing states',
     author='iMom0',
     author_email='mobeiheart@gmail.com',
     url='https://github.com/imom0/django-state-field',
     license='BSD',
     test_suite="runtests.runtests",
+    test_requirements=['mock==1.0.1'],
     package_dir={'': 'state_field'},
     packages=find_packages('state_field'),
     zip_safe=False,
